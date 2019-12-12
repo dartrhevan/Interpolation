dd3d40546e61e0a90eda4a8399e7fc94bbf4b0a3
dartrhevan <vovasatunkin@mail.ru>
2019-12-12 22:24:45 +0500

    linear mms - first version

---
+++ src/Aprox.ts
@@ -0,0 +1,36 @@
+﻿import Point from "./Model/Point";
+import gausMethod from "./GausMethod"
+
+
+function linear(points: Point[]) {
+    const x = getXSum(points);
+    const y = getYSum(points);
+    const xsq = getXSqSum(points);
+    const b = (x * y - y * xsq) / (x - xsq * points.length);
+    const a = (x * y - b * x) / xsq;
+    return `y = ${a}x + ${b}`;
+}
+
+function polinomial(points: Point[]) {
+    return '';
+}
+
+function getXSum(points : Point[]) : number {
+    return points.map(p => p.x).reduce((t, c) => t += c);
+}
+
+function getXSqSum(points: Point[]): number {
+    return points.map(p => p.x * p.x).reduce((t, c) => t += c);
+}
+
+function getXCbSum(points: Point[]): number {
+    return points.map(p => p.x * p.x * p.x).reduce((t, c) => t += c);
+}
+
+function getYSum(points: Point[]): number {
+    return points.map(p => p.y).reduce((t, c) => t += c);
+}
+
+function getXYSum(points: Point[]): number {
+    return points.map(p => p.y * p.x).reduce((t, c) => t += c);
+}
---
+++ src/GausMethod.ts
@@ -0,0 +1,56 @@
+
+
+function directMove(sys: number[][]) { //прямой проход
+
+    function validateAndCorrect(sys: number[][]) {
+
+        function swapRows(row1: number[], row2: number[]) {
+            for (let i = 0; i < row1.length; i++) {
+                const temp = row1[i];
+                row1[i] = row2[i];
+                row2[i] = temp;
+            }
+        }
+
+        for (let j = 1; j < sys.length; j++) {
+            const row = sys.find(e => {
+                for (let i = 0; i < j; i++)
+                    if (e[i] != 0)
+                        return false;
+                return true;
+            });
+            if (row !== sys[j])
+                swapRows(row as number[], sys[j]);
+        }
+    }
+
+    for (let i = 0; i < sys.length - 1; i++) {
+        if (sys[i][i] === 0) continue;
+        for (let j = i + 1; j < sys.length; j++) {
+            const coef = sys[j][i] / sys[i][i];
+            for (let k = 0; k < sys[j].length; k++)
+                sys[j][k] = Number((sys[j][k] - sys[i][k] * coef).toFixed(15));
+        }
+    }
+    validateAndCorrect(sys);
+    console.log(sys);
+}
+
+function reverseMove(sys: number[][]) {//обратный проход
+    for (let i = sys.length - 1; i >= 0; i--) {
+        sys[i][sys.length] /= sys[i][i];
+        sys[i][i] = 1;
+        for (let j = i - 1; j >= 0; j--) {
+            const coef = sys[j][i] / sys[i][i];
+            for (let k = sys.length; k >= 0; k--) {
+                sys[j][k] = Number((sys[j][k] - sys[i][k] * coef).toFixed(15));
+            }
+        }
+    }
+    console.log(sys);
+}
+
+export default function gausMethod(sys: number[][]) {
+    directMove(sys);
+    reverseMove(sys);
+}
